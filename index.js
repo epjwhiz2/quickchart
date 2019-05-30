85d216a0e4096907587497908a5aee5e85b1c5b6
erik <erik@realtourvision.com>
2019-05-30 13:28:12 -0400

    added more options

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const qrcode = require('qrcode');
 const text2png = require('text2png');
 const winston = require('winston');
 const { NodeVM } = require('vm2');
-const request = require('request');;
+const request = require('request');
 
 const { addBackgroundColors } = require('./charts');
 
@@ -20,7 +20,7 @@ const logger = new (winston.Logger)({
 
 const app = express();
 
-const isDev = app.get('env') === 'development';
+const isDev = false;
 
 app.set('views', `${__dirname}/templates`);
 app.use(express.static('public'));
@@ -48,8 +48,301 @@ function failPng(res, msg) {
     }));
 }
 
+function createLabelsFromData(data, type){
+    var today = new Date(data.time * 1000);
+	var labels = [];
+	switch(type){
+		case '24hours':
+			for(i = 0; i < 24; i++){
+				var hour = i;
+				var ampm = 'am';
+				if(hour >= 12){
+					ampm = 'pm';
+					hour = hour - 12;
+				}
+				if(hour === 0){
+					hour = 12;
+				}
+				labels.push(hour + ampm);
+			}
+		break;
+		case '7days':
+			for(i = -8; i < -1; i++){
+				var day = new Date(today.getTime() + (1000 * 60 * 60 * 24 * i));
+				var month = day.getMonth() + 1;
+				if(month < 10){
+					month = '0' + month;
+				}
+				var date = day.getDate() + 1;
+				if(date < 10){
+					date = '0' + date;
+				}
+				day = day.getFullYear() + '-' + month + '-' + date;
+				labels.push(day);
+			}
+		break;
+		case '9weeks':
+			for(i = -9; i < 0; i++){				
+				var day2 = new Date(today.getTime() - (1000 * 60 * 60 * 24 * 2) + (1000 * 60 * 60 * 24 * 7 * (i + 1)));
+				var day = new Date(day2.getTime() - (1000 * 60 * 60 * 24 * 6));
+				var month = day.getMonth() + 1;
+				if(month < 10){
+					month = '0' + month;
+				}
+				var date = day.getDate() + 1;
+				if(date < 10){
+					date = '0' + date;
+				}
+				var month2 = day2.getMonth() + 1;
+				if(month2 < 10){
+					month2 = '0' + month2;
+				}
+				var date2 = day2.getDate() + 1;
+				if(date2 < 10){
+					date2 = '0' + date2;
+				}
+				
+				day = month + '/' + date + ' - ' + month2 + '/' + date2 + '        ';
+				labels.push(day);
+			}
+		break;
+		default:
+			data.values.forEach(function(v){
+				labels.push('');
+			});
+		break;
+	}
+	return labels;
+}
+
+function constructChart(data){
+    var arrAllowedTypes = [
+        'pie',
+        'doughnut',
+        'line',
+        'bar'
+    ];
+    if(arrAllowedTypes.indexOf(data.template) >= 0){	
+		var colorIndex = 0;
+		var getColor = function(){
+			var arrColor = [
+				[244,196,149,0,1,2],
+				[193,168,239,2,0,1],
+				[235,166,183,0,2,1],
+				[255,205,86,0,1,2],
+				[231,219,160,1,0,2],
+				[154,194,212,2,1,0]
+			];
+			
+			var loopCount = 0;
+			var rowIndex = 0;
+			if(colorIndex > 0){
+				loopCount = Math.floor(colorIndex / arrColor.length);
+				rowIndex = colorIndex - (loopCount * arrColor.length);
+			}
+			var baseColor = arrColor[rowIndex];
+			
+			baseColor[baseColor[3]] = baseColor[baseColor[3]] - (loopCount * 43);
+			baseColor[baseColor[4]] = baseColor[baseColor[4]] - (loopCount * 31);
+			baseColor[baseColor[5]] = baseColor[baseColor[5]] - (loopCount * 17);
+			
+			if(baseColor[0] < 50){
+				baseColor[0] = baseColor[0] + 205;
+			}
+			if(baseColor[1] < 50){
+				baseColor[1] = baseColor[1] + 205;
+			}
+			if(baseColor[2] < 50){
+				baseColor[2] = baseColor[2] + 205;
+			}
+			
+			if(baseColor[0] > 235){
+				baseColor[0] = baseColor[0] - 15;
+			}
+			if(baseColor[1] > 235){
+				baseColor[1] = baseColor[1] - 15;
+			}
+			if(baseColor[2] > 235){
+				baseColor[2] = baseColor[2] - 15;
+			}
+			
+			colorIndex = colorIndex + 1;
+			
+			return '#' + toHex(baseColor[0]) + toHex(baseColor[1]) + toHex(baseColor[2]);
+		}
+	
+		data.colors = [];
+		data.values.forEach(function(){
+			data.colors.push(getColor());
+		});
+		
+		switch(data.template){
+			case 'line':
+				return constructLineChart(data);
+			break;
+			case 'bar':
+				return constructBarChart(data);
+			break;
+			case 'pie':
+				return constructPieChart(data);
+			break;
+			case 'doughnut':
+				return constructDoughnutChart(data);
+			break;
+		}
+    }
+    return false;
+}
+
+function constructBarChart(data){
+		
+    return {
+        'type' : 'bar',
+        'data' : {
+            'labels' : data.labels,            
+            'datasets' : [
+				{
+                    'label' : '',
+                    'data' : data.values,
+                    'backgroundColor' : data.colors
+                }
+			]
+        },
+        'options' : {
+            'legend' : {
+                'display' : false
+            },
+            'scales' : {
+                'xAxes' : [
+					{
+                        'ticks' : {
+                            'fontSize' : 8,
+                        },
+                        'gridLines' : {
+                            'display' : false
+                        }
+                    }
+				],
+                'yAxes' : [
+					{
+                        'ticks' : {
+                            'fontSize' : 8
+                        },
+                        'gridLines' : {
+                            'display' : false
+                        }
+                    }
+                ]
+			}
+        }
+    };
+}
+
+function constructLineChart(data){
+    var retObj = {
+        'type' : 'line',
+        'data' : {
+            'labels' : data.labels,            
+            'datasets' : [
+				{
+                    'data' : data.values,
+                }
+			]
+        },
+		'options' : {
+			'legend' : {
+				'display' : false
+			}
+		}
+	};
+	if(data.labeltype && data.labeltype === '9weeks'){
+		retObj.options.scales = {};
+		retObj.options.scales.xAxes = [];
+		retObj.options.scales.xAxes.push({
+			'scaleLabel' : {
+				'labelString' : 'Time Period',
+				'display' : true
+			}
+		});
+		retObj.options.scales.yAxes = [];
+		retObj.options.scales.yAxes.push({
+			'scaleLabel' : {
+				'labelString' : 'Visits',
+				'display' : true
+			}
+		});
+	}
+	return retObj;
+}
+
+function toHex(a){
+	var hex = a.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+
+function constructPieChart(data){
+    return {
+        'type' : 'doughnut',
+        'data' : {
+            'labels' : data.labels,            
+            'datasets' : [
+				{
+                    'data' : data.values,
+                    'backgroundColor' : data.colors
+                }
+			]
+        },
+        'options' : {
+            'legend' : {
+                'display' : false,
+				'labels' : {
+					'fontSize' : 6,
+					'boxWidth' : 10,
+					'padding' : 8
+				}
+            },
+			'plugins' : {
+				'datalabels' : {
+					'display' : false
+				}
+			}
+        }
+    };
+}
+
+function constructDoughnutChart(data){
+		
+    return {
+        'type' : 'doughnut',
+        'data' : {
+            'labels' : data.labels,            
+            'datasets' : [
+				{
+                    'data' : data.values,
+                    'backgroundColor' : data.colors
+                }
+			]
+        },
+        'options' : {
+            'legend' : {
+                'display' : false,
+				'labels' : {
+					'fontSize' : 6,
+					'boxWidth' : 10,
+					'padding' : 8
+				}
+            },
+			'plugins' : {
+				'datalabels' : {
+					'display' : false
+				}
+			}
+        }
+    };
+}
+
 app.get('/chart', (req, res) => {
-    if (!req.query.c && !req.query.url && !req.query.t) {
+    if (!req.query.c && !req.query.url && !req.query.t && !req.query.z) {
         failPng(res, 'Invalid request');
         return;
     }
@@ -83,6 +376,19 @@ app.get('/chart', (req, res) => {
                 values : JSON.parse(decodeURIComponent(req.query.v)),
                 colors : JSON.parse(decodeURIComponent(req.query.z))
             }
+        } else if(req.query.z) {
+            untrustedInput = {
+                template : decodeURIComponent(req.query.z),
+                values : req.query.x.split(',')
+			};
+			if(req.query.w){
+				untrustedInput.time = parseInt(req.query.v);
+				untrustedInput.labeltype = decodeURIComponent(req.query.y);
+				untrustedInput.labels = createLabelsFromData(untrustedInput, untrustedInput.labeltype);
+			} else {
+				untrustedInput.labeltype = decodeURIComponent(req.query.y);
+				untrustedInput.labels = createLabelsFromData(untrustedInput, 'default');
+			}
         } else {
             untrustedInput = decodeURIComponent(req.query.url);
         }
@@ -102,126 +408,8 @@ app.get('/chart', (req, res) => {
             const vm = new NodeVM();
             chart = vm.run(`module.exports = ${untrustedInput}`);
             processChart(chart);
-        } else if(req.query.t) {
-            switch(untrustedInput.template) {
-                case 'pie':
-                    chart = {
-                        type : 'pie',
-                        data : {
-                            labels : untrustedInput.labels,            
-                            datasets : [
-                                {
-                                    data : untrustedInput.values,
-                                    backgroundColor : untrustedInput.colors
-                                }
-                            ]
-                        },
-                        options : {
-                            legend : {
-                                display : false,
-                                labels : {
-                                    fontSize : 6,
-                                    boxWidth : 10,
-                                    padding : 8
-                                }
-                            },
-                            plugins : {
-                                datalabels : {
-                                    display : false
-                                }
-                            }
-                        }
-                    };
-                break;
-                case 'doughnut':
-                    chart = {
-                        type : 'doughnut',
-                        data : {
-                            labels : untrustedInput.labels,            
-                            datasets : [
-                                {
-                                    data : untrustedInput.values,
-                                    backgroundColor : untrustedInput.colors
-                                }
-                            ]
-                        },
-                        options : {
-                            legend : {
-                                display : false,
-                                labels : {
-                                    fontSize : 6,
-                                    boxWidth : 10,
-                                    padding : 8
-                                }
-                            },
-                            plugins : {
-                                datalabels : {
-                                    display : false
-                                }
-                            }
-                        }
-                    };
-                break;
-                case 'bar':
-                    chart = {
-                        type : 'bar',
-                        data : {
-                            labels : untrustedInput.labels,            
-                            datasets : [
-                                {
-                                    data : untrustedInput.values,
-                                    backgroundColor : untrustedInput.colors
-                                }
-                            ]
-                        },
-                        options : {
-                            legend : {
-                                display : false
-                            },
-                            scales : {
-                                xAxes: [
-                                    {
-                                        ticks:{
-                                            fontSize : 8
-                                        },
-                                        gridLines:{
-                                            display : false
-                                        }
-                                    }
-                                ],                                
-                                yAxes: [
-                                    {
-                                        ticks:{
-                                            fontSize : 8
-                                        },
-                                        gridLines:{
-                                            display : false
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    };
-                break;
-                case 'line':
-                    chart = {
-                        type : 'line',
-                        data : {
-                            labels : untrustedInput.labels,            
-                            datasets : [
-                                {
-                                    data : untrustedInput.values
-                                }
-                            ]
-                        },
-                        options : {
-                            legend : {
-                                display : false
-                            }
-                        }
-                    };
-                break;
-            }
+        } else if(req.query.z) {
+            chart = constructChart(untrustedInput);
             processChart(chart);
         } else {
             request({rejectUnauthorized: false, url: untrustedInput, timeout: 10000}, function (error, response, body) {
